929f809a8915d87f9e8cb929ee1499f837ef1136
scherbatyukk <itsfranplay@gmail.com>
2023-01-08 15:12:47 +0200

    done structure for lab3

---
+++ course_work/lab3/main.py
@@ -0,0 +1,26 @@
+import sys
+import pip._vendor.requests as requests
+
+# total arguments
+n = len(sys.argv)
+
+Request = "http://localhost:8888/" + sys.argv[2] + "?"
+
+for i in range(3, n):
+	Request += sys.argv[i]
+	if i != n-1:
+		Request += "&"
+
+response = ""
+if sys.argv[1] == "get":
+	response = requests.get(Request)
+if sys.argv[1] == "post":
+	response = requests.post(Request)
+if sys.argv[1] == "patch":
+	response = requests.patch(Request)
+if sys.argv[1] == "delete":
+	response = requests.delete(Request)
+
+
+print("Status code:", response.status_code)
+print("Response:", response.json())
